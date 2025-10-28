76705b4a49dd490ff9d297813b636b29e5221d80
djodysutiawan <djodysutiawan@gmail.com>
2025-10-28 20:44:11 +0700

    memperbaiki template dan penjaluran kerja tim

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,5 @@
 import 'package:flutter/material.dart';
 import 'pages/onboarding_page.dart';
-import 'pages/login_page.dart';
-import 'pages/register_page.dart';
-import 'pages/home_page.dart';
-// import 'pages/explore_page.dart';
-// import 'pages/detail_page.dart';
-// import 'pages/booking_page.dart';
-// import 'pages/checkout_page.dart';
-// import 'pages/mybooking_page.dart';
-// import 'pages/favorite_page.dart';
-// import 'pages/profile_page.dart';
-// import 'pages/help_page.dart';
 
 void main() {
   runApp(const TempatinApp());
@@ -39,19 +28,7 @@ class TempatinApp extends StatelessWidget {
       initialRoute: '/onboarding',
 
       // Daftar semua rute di aplikasi
-      routes: {
-        '/onboarding': (context) => const OnboardingPage(),
-        '/login': (context) => const LoginPage(),
-        '/register': (context) => const RegisterPage(),
-        '/home': (context) => const HomePage(),
-        // '/explore': (context) => const ExplorePage(),
-        // '/detail': (context) => const DetailPage(),
-        // '/booking': (context) => const BookingPage(),
-        // '/checkout': (context) => const CheckoutPage(),
-        // '/mybooking': (context) => const MyBookingPage(),
-        // '/profile': (context) => const ProfilePage(),
-        // '/help': (context) => const HelpPage(),
-      },
+      routes: {'/onboarding': (context) => const OnboardingPage()},
     );
   }
 }
